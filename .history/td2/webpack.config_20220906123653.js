77d1ce3fb7d59787538496f63d2404ec7be2fb42
Michaël Nass <michael.nass@free.fr>
2022-09-06 13:10:52 +0200

    TD2

---
+++ .history/td2/webpack.config_20220906123653.js
@@ -0,0 +1,27 @@
+// updated webpack.config.js that integrates babel to assets pipeline.
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  target: ['web', 'es5'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            targets: 'ie >= 11',
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
